5f94abd49ed6c5ec0179a715afde12236e789741
Jai Ram Rideout <jai.rideout@gmail.com>
2015-12-12 00:23:42 -0700

    Fix workshop setup script permission issues.

--- mahidol-university-thailand-2015/workshop-setup.sh
+++ mahidol-university-thailand-2015/workshop-setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
-sudo mkdir -p --mode a+rwx /mnt/workshop/cli /mnt/temp
+sudo mkdir -p --mode a+rwx /mnt/workshop /mnt/temp
 sed -i -e's?temp_dir\t.*$?temp_dir\t/mnt/temp/?' /qiime_software/qiime_config
 cd /mnt/workshop
+mkdir cli
 wget https://github.com/biocore/qiime-workshops/raw/master/mahidol-university-thailand-2015/commands/workshop-exercises.ipynb -O workshop-exercises.ipynb
